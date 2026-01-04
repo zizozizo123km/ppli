f56828c5223947921780b68a2a7fd89f0fa3ac67
zizozizo123km <nnacar12@gmail.com>
2026-01-04 19:44:37 +0100

    Architect: resolve dependencies for src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css' // Tailwind CSS base imports
+import './App.css' // Custom application styles
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
